03ad8f5f5e35946e55646cfbaab07a1a247d4794
Nicolas Vermande <vfiftyfive@gmail.com>
2020-01-29 19:10:33 +0000

    import vms

--- import.sh
+++ import.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-terraform import vsphere_distributed_virtual_switch.legacy-VDS /uktme-01/network/legacy-VDS
-terraform import vsphere_distributed_port_group.net_1 /uktme-01/network/net_1
-terraform import vsphere_distributed_port_group.net_2 /uktme-01/network/net_2
+terraform import vsphere_virtual_machine.vmus-1 /uktme-01/vm/vmus-1
+terraform import vsphere_virtual_machine.vmus-2 /uktme-01/vm/vmus-2
